42774a04204833d18bf0e3153d66307db5f8f3dc
Lucio Merotta <lucio@swibeco.ch>
2020-12-21 10:08:29 +0100

    Login flow

---
+++ symfony/src/Modules/UserManagement/Action/LoginAction.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Modules\UserManagement\Action;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginAction
+{
+    /**
+     * @Route("/security/authentication/login", name="app.security.action.login", methods={"POST"})
+     */
+    public function action(): JsonResponse
+    {
+        return new JsonResponse([], JsonResponse::HTTP_NO_CONTENT);
+    }
+}
