c1083b9ce8a550c901bc85248305332e8e882de1
Lucio Merotta <lucio@swibeco.ch>
2020-12-21 09:29:30 +0100

    Fixer

--- symfony/src/Entity/Security/User.php
+++ symfony/src/Entity/Security/User.php
@@ -3,13 +3,13 @@
 namespace App\Entity\Security;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use App\Modules\UserManagement\Messenger\Commands\SignUserUp;
 use App\Repository\Security\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use App\Modules\UserManagement\Messenger\Commands\SignUserUp;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
